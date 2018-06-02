1c673e935af7e6c297ea0ffaa7b766f8ea5c40de
melkashef <melkashef@e-biz.fr>
2018-06-02 15:16:11 +0200

    Handle item clicked to launch note activity

--- NoteKeeper/app/src/main/java/mo/takima/fr/notekeeper/NoteListActivity.java
+++ NoteKeeper/app/src/main/java/mo/takima/fr/notekeeper/NoteListActivity.java
@@ -1,11 +1,13 @@
 package mo.takima.fr.notekeeper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import java.util.List;
@@ -36,5 +38,12 @@ public class NoteListActivity extends AppCompatActivity {
 
     ArrayAdapter<NoteInfo> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,notes );
     listNotes.setAdapter(adapter);
+
+    listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
+        startActivity(intent);
+      }
+    });
   }
 }
