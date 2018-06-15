be4e78b1c1a5d5585ec78f6c75170c1333b5222b
Melkashef <melkashef@e-biz.fr>
2018-06-15 17:34:51 +0200

    Implementing test

--- NoteKeeper/app/src/test/java/mo/takima/fr/notekeeper/DataManagerTest.java
+++ NoteKeeper/app/src/test/java/mo/takima/fr/notekeeper/DataManagerTest.java
@@ -5,6 +5,14 @@ import org.junit.Test;
 public class DataManagerTest {
 
   @Test public void createNewNote() {
+    DataManager dm = DataManager.getInstance();
+    final CourseInfo courseInfo = dm.getCourse("android_async");
+    final String noteTitle = "Test note title";
+    final String noteText = "This is the body text of my test note";
 
+    int noteIndex = dm.createNewNote();
+    NoteInfo noteInfo = dm.getNotes().get(noteIndex);
+    noteInfo.setCourse(courseInfo);
+    noteInfo.setTitle(noteTitle);
   }
 }
