5411dd4a1ee583ce8cddc919ea41a17ef0b47a9d
Melkashef <melkashef@e-biz.fr>
2018-07-16 17:33:35 +0200

    Adding setup method for testing

--- NoteKeeper/app/src/test/java/mo/takima/fr/notekeeper/DataManagerTest.java
+++ NoteKeeper/app/src/test/java/mo/takima/fr/notekeeper/DataManagerTest.java
@@ -1,10 +1,18 @@
 package mo.takima.fr.notekeeper;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class DataManagerTest {
+
+  @Before
+  public void setUp() throws Exception {
+    DataManager dm = DataManager.getInstance();
+    dm.getNotes().clear();
+    dm.initializeExampleNotes();
+  }
 
   @Test public void createNewNote() {
     DataManager dm = DataManager.getInstance();
@@ -22,5 +30,32 @@ public class DataManagerTest {
     assertEquals(courseInfo,compareNote.getCourse());
     assertEquals(noteTitle,compareNote.getTitle());
     assertEquals(noteText,compareNote.getText());
+  }
+
+  @Test public void findSimilarNotes() {
+    DataManager dm = DataManager.getInstance();
+    final CourseInfo courseInfo = dm.getCourse("android_async");
+    final String noteTitle = "Test note title";
+    final String noteText1 = "This is the body text of my test note";
+    final String noteText2 = "This is the body of my second test note";
+
+    int noteIndex1 = dm.createNewNote();
+    NoteInfo noteInfo1 = dm.getNotes().get(noteIndex1);
+    noteInfo1.setCourse(courseInfo);
+    noteInfo1.setTitle(noteTitle);
+    noteInfo1.setText(noteText1);
+
+    int noteIndex2 = dm.createNewNote();
+    NoteInfo noteInfo2 = dm.getNotes().get(noteIndex2);
+    noteInfo2.setCourse(courseInfo);
+    noteInfo2.setTitle(noteTitle);
+    noteInfo2.setText(noteText2);
+
+    int foundIndex1 = dm.findNote(noteInfo1);
+    assertEquals(noteIndex1,foundIndex1);
+
+    int foundIndex2 = dm.findNote(noteInfo2);
+    assertEquals(noteIndex2,foundIndex2);
+
   }
 }
